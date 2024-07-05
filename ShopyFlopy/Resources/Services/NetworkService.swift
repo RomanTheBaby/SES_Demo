52f18c0f3836120bb428f469381ab5916a26a0a5
RomanTheBaby <mailofrom@gmail.com>
2024-07-04 21:56:49 -0600

    more updates

--- ShopyFlopy/Resources/Services/NetworkService.swift
+++ ShopyFlopy/Resources/Services/NetworkService.swift
@@ -9,26 +9,26 @@ import Foundation
 
 class NetworkService {
     
+    private let decoder: JSONDecoder
+
+    init() {
+        decoder = JSONDecoder()
+    }
+    
     func makeRequest(_ urlRequest: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
         // TODO: Adding this is a task
-        let hardcodedInfo = WeatherInformation(
-            cityName: "City",
-            temperature: "20º",
-            title: "Partly Cloudly",
-            description: "Slightly humid with gentle breeze of desparation because it's monday"
-        )
-        completion(try! JSONEncoder().encode(hardcodedInfo), nil)
+//        let hardcodedInfo = WeatherInformation(
+//            cityName: "City",
+//            temperature: "20º",
+//            title: "Partly Cloudly",
+//            description: "Slightly humid with gentle breeze of desparation because it's monday"
+//        )
+//        completion(try! JSONEncoder().encode(hardcodedInfo), nil)
+        // TODO: make a request somehow?
     }
     
-    func makeRequest(_ urlRequest: URLRequest) async throws -> Data {
-        // TODO: Adding this is a task
-        let hardcodedInfo = WeatherInformation(
-            cityName: "City",
-            temperature: "20º",
-            title: "Partly Cloudly",
-            description: "Slightly humid with gentle breeze of desparation because it's monday"
-        )
-        return try! JSONEncoder().encode(hardcodedInfo)
+    func makeRequest<ResponseModel: Codable>(_ urlRequest: URLRequest) async throws -> ResponseModel {
+        let (data, _) = try await URLSession.shared.data(for: urlRequest)
+        return try decoder.decode(ResponseModel.self, from: data)
     }
-    
 }
--- ShopyFlopy/Resources/Services/WeatherService.swift
+++ ShopyFlopy/Resources/Services/WeatherService.swift
@@ -10,50 +10,40 @@ import CoreLocation
 
 class WeatherService {
     
-    private let decoder: JSONDecoder
     private let networkService: NetworkService
     
     init() {
         networkService = NetworkService()
-        decoder = JSONDecoder()
         // TODO: Task where to add this as task
-//        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        //        decoder.keyDecodingStrategy = .convertFromSnakeCase
     }
     
-    func fetchWeather(_ weatherUpdateHandler: @escaping ((WeatherInformation) -> Void)) {
+    func fetchWeather() async throws -> WeatherInformation {
         guard let currentCoordinates = LocationManager.shared.currentLocation?.coordinate else {
-            return
+            fatalError("We do not have device location, this should never happen")
         }
         
         let requestString = "https://www.weatherspier.com?latitude=\(currentCoordinates.latitude)&latitude=\(currentCoordinates.latitude)"
         let url = URL(string: requestString)!
-        // TODO: add weak self as task??
-        networkService.makeRequest(URLRequest(url: url)) { data, error in
-            if let data, let weatherInfo: WeatherInformation = self.parseInfo(from: data) {
-                weatherUpdateHandler(weatherInfo)
+        let urlRequest = URLRequest(url: url)
+        return try await withCheckedThrowingContinuation { continuation in
+            networkService.makeRequest(urlRequest) { data, error in
+                //                let weatherInfo = try! self.decoder.decode(WeatherInformation.self, from: data!)
+                let hardcodedInfo = WeatherInformation(
+                    cityName: "City",
+                    temperature: "20º",
+                    title: "Partly Cloudly",
+                    description: "Slightly humid with gentle breeze of wind"
+                )
+                continuation.resume(returning: hardcodedInfo)
             }
         }
-    }
-    
-    func fetchWeather() async throws -> WeatherInformation? {
-        guard let currentCoordinates = LocationManager.shared.currentLocation?.coordinate else {
-            return nil
-        }
         
-        let requestString = "https://www.weatherspier.com?latitude=\(currentCoordinates.latitude)&latitude=\(currentCoordinates.latitude)"
-        let url = URL(string: requestString)!
-        // TODO: add weak self as task??
-        let data = try await networkService.makeRequest(URLRequest(url: url))
-        return try! decoder.decode(WeatherInformation.self, from: data)
+        //        return try await networkService.makeRequest(URLRequest(url: url))
     }
     
     func fetchHourlyForecast(_ completion: (() -> Void)) {
         
-    }
-    
-    private func parseInfo(from data: Data) -> WeatherInformation? {
-        // TODO: parse this later
-        try? decoder.decode(WeatherInformation.self, from: data)
     }
     
 }
